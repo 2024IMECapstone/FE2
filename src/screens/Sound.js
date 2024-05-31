cf96df25ee153ec1c1505b9f64984baa1aeb0744
Cho2unseo <escces00@gmail.com>
2024-06-01 02:57:39 +0900

    feat: 타임라인 UI 보완 필요

--- src/screens/Sound.js
+++ src/screens/Sound.js
@@ -7,52 +7,97 @@ const TimelineAPI = ({
     const [list, setList] = useState([
     {
         title: '타로가 배고픈 것 같아요',
+        description: '오후 5:00',
     },
     {
         title: '타로가 졸린 것 같아요',
+        description: '오후 3:00',
     },
     {
         title: '타로가 배앓이 중인 것 같아요',
+        description: '오후 1:00',
     },
     {
         title: '타로가 트름하고 싶은 것 같아요',
+        description: '오후 12:00',
     },
     ]);
 
+    const renderDetail = (rowData) => {
+        const { title, description } = rowData;
+        return (
+            <View style = {styles.rowDetail}>
+                <Text style = {styles.titleStyle}>{title}</Text>
+                <Text style = {styles.descriptionStyle}>{description}</Text>
+            </View>
+        )
+    }
+
     return (
-        <Timeline
-            data={list}
-            circleSize={14}
-            circleColor='#FFFACD'
-            lineColor='#D9D9D9'
-            lineWidth={3}
-            descriptionStyle={styles.descriptionStyle}
-            timeContainerStyle={{ minWidth: 52, marginTop: -5 }}
-            descriptionStyle={{color:'black'}}
-            options={{
-                style: {paddingTop: 16}
-            }}
-        />
-    )
+        <View style = {styles.timelineContainer}>
+            <Timeline
+                showTime={false}
+                data={list}
+                circleSize={14}
+                circleColor='#FFFACD'
+                lineColor='#D9D9D9'
+                lineWidth={3}
+                timeContainerStyle={{ minWidth: 0}}
+                descriptionStyle={{color:'black'}}
+                renderDetail = {renderDetail}
+
+            />
+        </View>
+    );
 }
 
 export default function Sound() {
   return (
-    <View style={styles.container}>
         <TimelineAPI />
-    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    padding: 16,
+    // flex: 100,
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  timelineContainer: {
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 16,
+    shadowColor: '#000',
+    shadowOffset: { width:0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    elevation: 5,
+    height: 179,
+    margin: 16,
+  },
+  detailContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    paddingBottom: 20,
+    justifyContent: 'space-between',
+  },
+  rowDetail: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
   descriptionStyle: {
-    color: '#000',
+    color: 'gray',
     fontSize: 10,
+    // marginTop : 5,
+    textAlign: 'right',
+    flex: 1,
+    marginRight: 16,
+  },
+  titleStyle: {
+    color: 'black',
+    fontSize: 12,
+    marginRight: 10,
   },
 });
